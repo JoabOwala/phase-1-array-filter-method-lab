7a3b60b6ce848e400ab0ae3af91cdf399bfc97ce
Joab Owala <owalajoab@gmail.com>
2023-03-20 02:00:48 +0300

    Assignments completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here
+const findMatching = (driver,drivers) => driver.filter(driver => driver.toLowerCase() === drivers.toLowerCase())
+
+
+const fuzzyMatch = (driver,drivers) => driver.filter(driver => driver.charAt(0) === drivers.charAt(0))
+
+
+const matchName = (driver,drivers) => driver.filter(driver => driver.name === drivers)
